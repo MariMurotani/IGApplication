91f8593d93bebdb7bd277e4b9784435fdc209e6a
Mari Murotani <muromari5123@gmail.com>
2016-12-18 15:17:52 +0900

    1st Commit

---
+++ App/Containers/TestScreen.js
@@ -0,0 +1,24 @@
+// @flow
+
+import React from 'react'
+import { View, ScrollView, Text, Image } from 'react-native'
+import { Colors, Fonts, Images } from '../Themes'
+import R from 'ramda'
+
+// Styles
+import styles from './Styles/ThemeScreenStyle'
+
+// Colors
+const colors = R.keys(Colors)
+// Font Types
+const types = R.keys(Fonts.type)
+// Font Styles
+const fontStyles = R.keys(Fonts.style)
+
+export default class TestScreen extends React.Component {
+  render () {
+    return (
+      <Text>Hello world!</Text>
+    )
+  }
+}
