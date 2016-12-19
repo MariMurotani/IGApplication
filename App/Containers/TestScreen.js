380efa9ca3b3672e9fa6350c4f08889aa2c8540a
Mari Murotani <muromari5123@gmail.com>
2016-12-19 23:46:03 +0900

    Added data loading

--- App/Containers/TestScreen.js
+++ App/Containers/TestScreen.js
@@ -1,11 +1,13 @@
 // @flow
+'use strict';
 
 import React from 'react'
-import { View, ScrollView, Text, Image } from 'react-native'
+import { AppRegistry,View, ScrollView, Text, Image, ListView, StyleSheet } from 'react-native'
 import { Colors, Fonts, Images } from '../Themes'
 import R from 'ramda'
 
 // Styles
+//import styles from './Styles/DeviceInfoScreenStyle'
 import styles from './Styles/ThemeScreenStyle'
 
 // Colors
@@ -15,11 +17,64 @@ const types = R.keys(Fonts.type)
 // Font Styles
 const fontStyles = R.keys(Fonts.style)
 
+// URL feed
+var REQUEST_URL = "https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&num=200&q=http://feeds.reuters.com/reuters/businessNews";
+
+/*
+* http://facebook.github.io/react-native/releases/0.19/docs/tutorial.html
+* */
+// TestScreenScene
 export default class TestScreen extends React.Component {
+  //  コンストラクタ
+  constructor(props) {
+    super(props);
+    //  ステートの設定
+    this.state = {feeds: false};
+    //  データ取得
+    this.fetchData();
+  }
+  //  データ取得用
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        this.setState({
+          //  ステートに値を設定
+          feeds: responseData.feed,
+        });
+      })
+      .done();
+  }
+  //  描画用のrender
   render () {
+    if (!this.state.feed) {
+      return this.renderLoadingView();
+    }
     return (
       <View style={styles.mainContainer}>
-        <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      </View>
+    );
+    var feeds = this.state.feed;
+    console.log(feeds);
+    return this.renderListView(feeds);
+  }
+  //  ローディング用の表示をする
+  renderLoadingView(){
+    return (
+      <View style={styles.mainContainer}>
+        <Text>Loading......</Text>
+      </View>
+
+    );
+  }
+  //  リスト用の表示をする
+  renderListView(feeds){
+    return (
+      <View style={styles.mainContainer}>
+        <Text>{feeds.title}</Text>
+        <Text>{feeds.description}</Text>
       </View>
     )
   }
